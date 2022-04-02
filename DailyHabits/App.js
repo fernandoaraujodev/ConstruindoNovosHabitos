0d3835908f68e757ac222b9603029791e4d208a6
Fernando Arauj <fernandovcaraujo@gmail.com>
2022-04-01 23:55:08 -0300

    d

--- DailyHabits/App.js
+++ DailyHabits/App.js
@@ -21,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#452574',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
